2999444bb7fd0398307f5eb5eabea9dc34712e45
Divij Bindlish <dvjbndlsh93@gmail.com>
2013-10-19 22:26:20 +0530

    Adds configuration options for express js

--- app.js
+++ app.js
@@ -9,6 +9,17 @@ var app = express();
 var server = http.createServer(app);
 var io = websocket.listen(server);
 
+app.set('port', process.env.PORT || 3000);
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(express.cookieParser());
+app.use(express.session({secret: 'Avada Kedavra'}));
+app.use(app.router);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
 app.get('/', function(req, res) {
   res.send('Hello World!');
 });
